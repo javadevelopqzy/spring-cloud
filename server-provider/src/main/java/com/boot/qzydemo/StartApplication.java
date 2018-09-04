60e9a837c678d144568eaac38972bc0885c9951d
Administrator <1252525229@qq.com>
2018-09-04 23:57:32 +0800

    初始化项目

--- server-provider/src/main/java/com/boot/qzydemo/StartApplication.java
+++ server-provider/src/main/java/com/boot/qzydemo/StartApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 class StartApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
