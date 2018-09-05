d727bd0f564965447e9fec8048b167a6f56f459e
Administrator <1252525229@qq.com>
2018-09-05 23:49:54 +0800

    初始化项目

---
+++ server-consumer/src/main/java/com/boot/qzydemo/StartApplication.java
@@ -0,0 +1,24 @@
+package com.boot.qzydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+// 服务端发现客户端
+@EnableDiscoveryClient
+@SpringBootApplication
+class StartApplication {
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+}
